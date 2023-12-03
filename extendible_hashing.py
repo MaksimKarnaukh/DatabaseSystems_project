d15a107e19303d16a866bb3a5c4bbfd915da7709
MaksimKarnaukh <maksim.karnaukh@student.uantwerpen.be>
2023-12-03 13:38:29 +0100

    Changes by elthomaso

--- extendible_hashing.py
+++ extendible_hashing.py
@@ -178,7 +178,6 @@ class Bucket(object):
 
         local_prefix_size = int.from_bytes(byte_data[0:1], byteorder='big')
         max_size = int.from_bytes(byte_data[1:2], byteorder='big')
-        # TODO: READ current values list size
         cur_size = int.from_bytes(byte_data[2:3], byteorder='big')
         bucket_id = int.from_bytes(byte_data[3:list_start_byte], byteorder='big')
 
@@ -265,6 +264,18 @@ def get_hash_prefix(keyHash: str, prefixSize: int) -> str:
     return keyHash[: prefixSize]
 
 
+class BucketWrapper:
+    """A class that wraps a Bucket, so that a Bucket object cab
+    be evicted from memory or loaded into memory in one operation.
+
+    Setting the contents of the wrapper makes it so that every
+    prefix that is related to a specific Bucket, now refers to
+    the same passed Bucket object or bucket ID.
+    """
+    def __init__(self, initial_content: Union[Bucket, int]):
+        self.contents: Union[Bucket, int] = initial_content
+
+
 class ExtendibleHashingIndex(object):
     def __init__(self):
 
@@ -275,15 +286,27 @@ class ExtendibleHashingIndex(object):
         self.bucketsDataFileName: str = "buckets_data.dat"
         self.bucketsIDCounter: int = 0
 
-        self.bucketPointers: Dict[str: Union[int, Bucket]] = {
-            "0": Bucket(self.reserve_bucket_ID()),
-            "1": Bucket(self.reserve_bucket_ID())
+        bucket0: Bucket = Bucket(self.reserve_bucket_ID())
+        bucket1: Bucket = Bucket(self.reserve_bucket_ID())
+        bucketWrapper0: BucketWrapper = BucketWrapper(bucket0)
+        bucketWrapper1: BucketWrapper = BucketWrapper(bucket1)
+        self.bucketPointers: Dict[str: Union[int, BucketWrapper]] = {
+            "0": bucketWrapper0,
+            "1": bucketWrapper1
+        }
+        # List of bucket IDs
+        self.bucketsInMemory: List[Bucket] = [bucket0, bucket1]
+        # mapping from bucket ID to BucketWrapper
+        self.bucketsToWrapper: Dict[int, BucketWrapper] = {
+            bucket0.bucketID: bucketWrapper0,
+            bucket1.bucketID: bucketWrapper1,
         }
 
     def __str__(self):
         reversedDict = dict()
         bucketByID = dict()
         for k, v in self.bucketPointers.items():
+            v = v.contents if isinstance(v.contents, Bucket) else self.read_bucket(v.contents)
             bucketByID[v.bucketID] = v
             if v.bucketID in reversedDict.keys():
                 reversedDict[v.bucketID].append(k)
@@ -308,13 +331,18 @@ class ExtendibleHashingIndex(object):
         self.bucketsIDCounter += 1
         return oldValue
 
-    def get_bucket(self, prefix: str) -> Union[Bucket, None]:
+    def get_bucket(self, prefix: str) -> Tuple[Union[Bucket, None], Union[BucketWrapper, None]]:
         """Retrieve the bucket corresponding to the given prefix.
 
         :param prefix: A prefix of the full key hash to find the bucket for
-        :return: The corresponding Bucket if it exists, else None
+        :return: (
+            The corresponding Bucket if it exists, else None,
+            The corresponding BucketWrapper if it exists, else None,
+        )
         """
-        bucket: Union[int, Bucket] = self.bucketPointers.get(prefix, None)
+        bucket_wrapper: BucketWrapper = self.bucketPointers.get(prefix, None)
+        assert bucket_wrapper is not None, f"Invalid prefix was used to get a bucket, no {BucketWrapper.__class__.__name__} was found for the prefix '{prefix}'"
+        bucket: Union[int, Bucket] = bucket_wrapper.contents
         if isinstance(bucket, int):
             bucket = self.read_bucket(bucket)
             bucket_wrapper.contents = bucket
@@ -327,8 +355,8 @@ class ExtendibleHashingIndex(object):
         """Store the given bucket or bucket ID under the given prefix.
         Evicts another bucket from memory if there is no more room in-memory.
 
-        :param keyHash: The full key hash to find the bucket for
-        :return: The corresponding Bucket if it exists, else None
+        :param prefix: The prefix of the full key hash to set the bucket or bucket ID for
+        :param bucketWrapper: The bucket wrapper to map the prefix to
         """
         wrapperContents: Union[Bucket, int] = bucketWrapper.contents
         bucketID: int = wrapperContents if isinstance(wrapperContents, int) else wrapperContents.bucketID
@@ -384,20 +412,20 @@ class ExtendibleHashingIndex(object):
         # first, get the bucket associated with the key
         keyHash: str = self.get_hash_from_key(key=key)
         prefix: str = self.get_prefix_from_key_hash(keyHash=keyHash)
-        bucket = self.get_bucket(prefix=prefix)
+        bucket, _ = self.get_bucket(prefix=prefix)
         # then, get the item from the bucket
         return bucket.search(keyHash)
 
     def insert_keyval(self, keyHash: str, value):
         """Inserts a key-value pair into the index."""
         prefix: str = self.get_prefix_from_key_hash(keyHash=keyHash)
-        bucket: Bucket = self.get_bucket(prefix=prefix)
+        bucket, bucketWrapper = self.get_bucket(prefix=prefix)
         bucketValue: BucketValue = BucketValue(keyHash, value)
         success: bool = bucket.insert(bucketValue)
 
         # Bucket is full, split it
         if not success:
-            self.split(bucket)
+            self.split(bucketWrapper)
 
             # insert recursively (for in the case that the destination bucket is still full)
             self.insert_keyval(keyHash, value)
@@ -411,42 +439,51 @@ class ExtendibleHashingIndex(object):
         # first, get the bucket associated with the key
         keyHash = self.get_hash_from_key(key=key)
         prefix: str = self.get_prefix_from_key_hash(keyHash=keyHash)
-        bucket = self.get_bucket(prefix=prefix)
+        bucket, _ = self.get_bucket(prefix=prefix)
         # then, delete the item from the bucket
         return bucket.delete(keyHash)
 
-    def split(self, bucket: Bucket) -> None:
+    def split(self, bucketWrapper: BucketWrapper) -> None:
         """Perfom a split on the index for a given bucket.
         Perform any necessary actions after the split to
         bring the index into a valid state again.
 
         IMPORTANT: This method invalidates the passed *bucket* reference
 
-        :param bucket: The bucket to split
+        :param bucketWrapper: The wrapper of the bucket to split
         """
         # TODO: \/ Buckets are stored in pages in memory???? \/
+        bucket: Bucket = bucketWrapper.contents
+        assert isinstance(bucket, Bucket), f"Can only split a bucket, not a '{bucket.__class__.__name__}' type"
+        assert bucket in self.bucketsInMemory, "Can only split a bucket that is in memory"
         shouldIncreaseGlobal: bool = bucket.localPrefixSize == self.globalHashPrefixSize
 
         # if the global prefix length is smaller than the prefix length after a split, then we need to
         # update the global prefix length and increase its length with 1, for each entry.
         if shouldIncreaseGlobal:
-            newBucketPointers: Dict[str: Union[Bucket, int]] = dict()
-            for oldPrefix, oldBucket in self.bucketPointers.items():
+            newBucketPointers: Dict[str: BucketWrapper] = dict()
+            for oldPrefix, oldBucketWrapper in self.bucketPointers.items():
                 newPrefix0, newPrefix1 = self.get_extended_prefixes(oldPrefix)
-                newBucketPointers[newPrefix0] = oldBucket
-                newBucketPointers[newPrefix1] = oldBucket
+                newBucketPointers[newPrefix0] = oldBucketWrapper
+                newBucketPointers[newPrefix1] = oldBucketWrapper
             self.bucketPointers = newBucketPointers
 
         res0, res1 = self.split_bucket(bucket)
         newBucket0, newBucketPrefix0 = res0
         newBucket1, newBucketPrefix1 = res1
 
+        bucketWrapper.contents = newBucket0
+        self.bucketsInMemory[self.bucketsInMemory.index(bucket)] = newBucket0
+        bucketWrapper0: BucketWrapper = bucketWrapper
+        bucketWrapper1: BucketWrapper = BucketWrapper(newBucket1)
+
+
         # because we did a split, we need to update all related pointers
         for ptr in self.bucketPointers.keys():
             if ptr.startswith(newBucketPrefix0):
-                self.bucketPointers[ptr] = newBucket0
+                self.set_bucket(ptr, bucketWrapper0)
             elif ptr.startswith(newBucketPrefix1):
-                self.bucketPointers[ptr] = newBucket1
+                self.set_bucket(ptr, bucketWrapper1)
 
         self.globalHashPrefixSize += shouldIncreaseGlobal
 
@@ -455,7 +492,7 @@ class ExtendibleHashingIndex(object):
         self.write_bucket(newBucket0)
         self.write_bucket(newBucket1)
 
-    def split_bucket(self, bucket: Bucket):
+    def split_bucket(self, bucket: Bucket) -> Tuple[Tuple[Bucket, str], Tuple[Bucket, str]]:
         """Create two new buckets based on an "old" bucket.
         The two new buckets contain the bucket values of
         the old bucket. The old bucket is untouched.
@@ -486,7 +523,7 @@ class ExtendibleHashingIndex(object):
             else:
                 print("YOU SHOULDN'T EVER SEE THIS MESSAGE")
 
-        return [new_bucket0, newPrefix0], [new_bucket1, newPrefix1]
+        return (new_bucket0, newPrefix0), (new_bucket1, newPrefix1)
 
     def get_extended_prefixes(self, prefix: str) -> Tuple[str, str]:
         """Convert the prefix into two extended prefixes of one 'bit' longer.
@@ -552,7 +589,7 @@ class ExtendibleHashingIndex(object):
         :return: A list of all violations
         """
         violations = []
-        for prefix, bucket in self.bucketPointers.items():
+        for prefix, bucketWrapper in self.bucketPointers.items():
             # A prefix is of the correct size
             isLenCorrect = len(prefix) == self.globalHashPrefixSize
             if not isLenCorrect:
@@ -561,6 +598,7 @@ class ExtendibleHashingIndex(object):
                     return violations
 
             element: BucketValue
+            bucket: Bucket = bucketWrapper.contents if isinstance(bucketWrapper.contents, Bucket) else self.read_bucket(bucketWrapper.contents)
             for element in bucket.list:
                 if not element.key.startswith(prefix[0:bucket.localPrefixSize]):
                     violations.append(f"incorrect prefix: {prefix[0:bucket.localPrefixSize]} for bucket element: {element.key} with bucket localPrefixSize = {bucket.localPrefixSize}")
@@ -598,7 +636,7 @@ if __name__ == "__main__":
         eh.write_bucket(b0)
         eh.bucketPointers[b0_prefix] = b0.bucketID
         eh.read_bucket(b0.bucketID)
-        b = eh.get_bucket(b0_prefix)
+        b, _ = eh.get_bucket(b0_prefix)
         print(b)
         print(b.list[0].get_value())
         print([v for v in b.list[0].get_value()])
@@ -617,10 +655,12 @@ if __name__ == "__main__":
 
         # TESTING
         print(eh)
+
         print(eh.isValid())
         print(eh.getViolations(False))
         print(len(eh.bucketPointers.keys()), "  (unique #prefixes)")
-        print(len(set([b.bucketID for b in eh.bucketPointers.values()])), "  (unique #buckets)")
-        print(max([b.bucketID for b in eh.bucketPointers.values()]), "  (largest Bucket ID)")
-        print(sum([len(b.list) for _, b in eh.bucketPointers.items()]), " (total bucket items)")
+        print(len(set([(b.contents if isinstance(b.contents, Bucket) else eh.read_bucket(b.contents)).bucketID for b in eh.bucketPointers.values()])), "  (unique #buckets)")
+        print(max([(b.contents if isinstance(b.contents, Bucket) else eh.read_bucket(b.contents)).bucketID for b in eh.bucketPointers.values()]), "  (largest Bucket ID)")
+        print(sum([len((b.contents if isinstance(b.contents, Bucket) else eh.read_bucket(b.contents)).list) for _, b in eh.bucketPointers.items()]), " (total bucket items)")
+        print(len([v for v in eh.bucketPointers.values() if isinstance(v.contents, Bucket)]), "(#buckets in-mem)")
 
